cabc7e64f00fb30e1988a3ae7728b2a5116805d5
meghnam16 <meg16.mallik@gmail.com>
2025-04-14 21:17:33 +1000

    Clean task2_2.py
    edited plots, cleaned code, added comments

--- task2/task2_2.py
+++ task2/task2_2.py
@@ -3,72 +3,115 @@ import re
 import matplotlib.pyplot as plt
 
 def task2_2():
-    accident = pd.read_csv("a1-datasets/accident.csv")
+    accident = pd.read_csv("accident.csv")
 
-    accident_times = accident['ACCIDENT_TIME']
-
+    # categorizes accident times acc. to TIME_OF_DAY categories
     accident['TIME_OF_DAY'] = accident['ACCIDENT_TIME'].apply(lambda x: sort_time(x))
+
+    '''
+    Plot 1
+    '''
 
     time_occurrences = {}
 
+    # frequency of each TIME_OF_DAY category occurrence
     for time in accident['TIME_OF_DAY']:
         if time not in time_occurrences:
             time_occurrences[time] = 1
         else:
             time_occurrences[time] += 1
 
+    # dataframe with number of accidents per time of day
     time_occurrences_df = pd.DataFrame(list(time_occurrences.items()), columns = ['TIME_OF_DAY', 'ACCIDENT_FREQUENCY'])
 
+    # set and sort TIME_OF_DAY elements as time appears in the day
     time_occurrences_df = time_occurrences_df.set_index('TIME_OF_DAY', drop=False)
     time_occurrences_df = time_occurrences_df.reindex(index=['Morning', 'Afternoon', 'Evening', 'Late Night'])
 
-    # bar chart
-    plt.title("Number of accidents by Time of Day")
+    plt.clf()
+    # plot bar chart
+    plt.figure()
+    plt.title("Number of Accidents by Time of Day")
     plt.bar(time_occurrences_df['TIME_OF_DAY'], time_occurrences_df['ACCIDENT_FREQUENCY'])
+    plt.xlabel("Time of Day")
+    plt.ylabel("Number of Accidents")
     plt.xticks(rotation=30)
+    plt.tight_layout(pad=2.0)
     plt.savefig('task2_2_timeofday.png')
 
-    # pie charts
+    ###################################################################################################################
 
-    words = {}
-    dca_desc_words = []
+    plt.clf()
+    '''
+    Plot 2
+    '''
 
-    for time in time_occurrences_df['TIME_OF_DAY']:
-        for desc in accident['DCA_DESC']:
+    word_counts = {}
+    dca_desc_top10_words = []
 
-            desc_split = re.findall(r'\b\w+\b', desc) # tokenize words
+    for time in time_occurrences_df.index:
+        time_filtered = accident[accident['TIME_OF_DAY'] == time]
+        for desc in time_filtered['DCA_DESC']:
 
-            for word in desc_split:
-                if word not in words:
-                    words[word] = 1
+            split_desc = re.findall(r'\b\w+\b', desc) # tokenize words
+
+            for word in split_desc:
+                if word not in word_counts:
+                    word_counts[word] = 1
                 else:
-                    words[word] += 1
+                    word_counts[word] += 1
         
         # arrange acc. to most frequently occurring words
-        words_sorted = sorted(list(words.items()), key=lambda x: x[1], reverse=True)
+        sorted_word_counts = sorted(list(word_counts.items()), key=lambda x: x[1], reverse=True)
         
         # store 10 most frequent words
-        dca_desc_words.append(words_sorted[:10])
-        words = {}
+        dca_desc_top10_words.append(sorted_word_counts[:10])
+        word_counts = {}
 
-    dca_desc_words_rearrange = []
-    for ls in dca_desc_words:
-        rearrange = list(map(list, zip(*ls)))
-        dca_desc_words_rearrange.append(rearrange)
+    '''
+    structure of dca_desc_top10_words:
+
+    [[(word1, count1), (word2, count2), ... for 'Morning'], 
+    [(word 1, count1), ... for 'Afternoon'], 
+    [for 'Evening'], [for 'Late Night']]
+
+    each sub-list corresponds to 1 of 4 pie charts.
+    '''
+
     
+    dca_desc_words_rearranged = []
+    for top_words in dca_desc_top10_words:
+        # for each sub_list containing 10 most frequent words in each TIME_OF_DAY
+        rearrange = list(map(list, zip(*top_words)))
+
+        # separates words and frequencies (removes tuple format)
+        dca_desc_words_rearranged.append(rearrange)
+    
+    # plot the pie charts
     titles = ['Morning', 'Afternoon', 'Evening', 'Late Night']
-    fig, axs = plt.subplots(2, 2)
-    fig.suptitle('10 most frequent accident terms by time of day')
+    fig, axs = plt.subplots(2, 2, figsize=(12, 10)) # 2x2 grid
+    fig.suptitle('10 Most Frequent Accident Terms by Time Of Day', fontsize=20)
+
     for i in range(2):
         for j in range(2):
+            # for each pie plot
             index = i * 2 + j
-            axs[i, j].pie(dca_desc_words_rearrange[index][1], labels=dca_desc_words_rearrange[index][0], autopct='%1.1f%%', textprops={'fontsize': 7})
-            axs[i, j].set_title(titles[index])
+            axs[i, j].pie(
+                dca_desc_words_rearranged[index][1], 
+                labels=dca_desc_words_rearranged[index][0], 
+                autopct='%1.1f%%', 
+                textprops={'fontsize': 10})
+            axs[i, j].set_title(titles[index], fontsize=16)
     plt.savefig('task2_2_wordpies.png')
 
-    # stacked bar charts
+    ###################################################################################################################
+    
+    plt.clf()
+
     '''
-    dataframe structure:
+    Plot 3
+
+    required dataframe structure:
 
                 monday      friday      sunday
 
@@ -79,12 +122,18 @@ def task2_2():
 
     days come from 'DAY_WEEK_DESC'
     '''
-    days = accident[accident['DAY_WEEK_DESC'].isin(['Monday', 'Friday', 'Sunday'])]
 
-    grouped = days.groupby(['DAY_WEEK_DESC', 'TIME_OF_DAY']).size().unstack(fill_value=0)
+    # days of the week for which data needs to be shown
+    select_days = accident[accident['DAY_WEEK_DESC'].isin(['Monday', 'Friday', 'Sunday'])]
+
+    selected_days_ordered = ['Monday', 'Friday', 'Sunday']
+
+    grouped = select_days.groupby(['DAY_WEEK_DESC', 'TIME_OF_DAY']).size().unstack(fill_value=0)
+    grouped = grouped.reindex(selected_days_ordered)
+    
     grouped.plot(kind='bar', stacked=True, figsize=(10, 6), colormap='Set2')
 
-
+    # plot stacked bar chart
     plt.title("Accidents by Time of Day across Days of the Week")
     plt.xlabel("Day of the Week")
     plt.ylabel("Number of Accidents")
@@ -93,7 +142,12 @@ def task2_2():
     plt.tight_layout()
     plt.savefig('task2_2_stackbar.png')
 
-# categorize accident time
+    ###################################################################################################################
+
+'''
+Function to categorize time with format HH:MM:SS (from 'ACCIDENT_TIME')
+'''
+
 def sort_time(time):
     time_format = r'\d{2}:\d{2}'
     if re.search(time_format, time):
@@ -107,11 +161,3 @@ def sort_time(time):
             return "Evening"
         else:
             return "Late Night"
-
-
-
-
-
-
-
-
